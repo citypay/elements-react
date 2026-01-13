3bdac8ffbf7ce0d09cfb6d0138782188036ee796
Sam Jauncey <sam.jauncey@citypay.com>
2026-01-13 10:22:00 +0000

    CE-97 Adds verify-auth route

---
+++ src/app/api/verify-auth/[intentId]/route.ts
@@ -0,0 +1,30 @@
+import {NextResponse} from "next/server";
+import {CityPay} from "@citypay/sdk";
+
+export async function GET(_req: Request, { params }:{params: {intentId: string}}) {
+
+    console.log('<> verify-auth')
+
+    const { intentId } = await params
+
+    const clientId = process.env.CITYPAY_CLIENT_ID
+    const licenceKey = process.env.CITYPAY_LICENCE_KEY
+    const mid = process.env.CITYPAY_MERCHANT_ID
+
+    if (!clientId || !licenceKey || !mid) {
+        return NextResponse.json({error: "Missing required environment variables"}, {status: 500});
+    }
+
+    const citypay = new CityPay(clientId, licenceKey, {
+        sandbox: true,
+    })
+
+    const result = await citypay.paymentIntents.verifyAuthorised({
+        payment_intent_id: intentId,
+    })
+
+    console.log(result)
+
+    return NextResponse.json(result, {status: 200})
+
+}
